1a5e951973e6032f7b8afb9bc650d5c7e9c3f8f6
Amogh Singh <asingh18_be23@thapar.edu>
2026-04-18 22:38:51 +0530

    added project changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const config = require("config");
+const dbgr = require("debug")("development:mongoose-connection");
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
+.connect(`${config.get("MONGODB_URL")}/scatch`)
 .then(function(){
-    console.log("Connected to MongoDB");
+    dbgr("Connected to MongoDB");
 })
 .catch(function(err){
-    console.log("Error connecting to MongoDB", err);
+    dbgr("Error connecting to MongoDB", err);
 });
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,27 @@
 const express =require("express");
 const router = express.Router();
 
+if(process.env.NODE_ENV === "development"){
+    router.get("/", async function(req, res){
+        let owners = await owmerModel.find();
+        if(owners.length > 0){
+            return res.status(503).send("you dont have permission to create a new owner")
+        }
+        let {fullname, email, password} = req.body;
+        let createdowner = await ownerModel.create({
+            fullname,            
+            email,
+            password,
+            
+        });
+    res.status(201).send (CreatedOwner);
+    });
+}
+
+
 router.get("/", function(req, res){
     res.send("hey its working");
 });
+
 
 module.exports = router;

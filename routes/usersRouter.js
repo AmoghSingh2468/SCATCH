ecc1a23e5f88dfed37c936c157990dd8f00e7a17
Amogh Singh <asingh18_be23@thapar.edu>
2026-05-04 01:22:46 +0530

    Added user register route and fixed error handling

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,7 @@
 const express =require("express");
 const router = express.Router();
+const bcrypt =require("bcrypt");
+const userModel =require("../models/userModel");
 
 router.get("/", function(req, res){
     res.send("hey its working");
@@ -7,6 +9,12 @@ router.get("/", function(req, res){
 roueter.post("/register", async function (res, res){
    try{
      let { email, password, fullname } =req.body;
+     bcrypt.gensalt(10, function(err,salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            if(err) return res.send(err.message);
+            else res.send(hash);
+     });
+    });
 
     let user =await userModel.create({
         email,
